3fd6ec06ae109af84a370ace0c4639290dfe8b1d
jayrathod26 <jkrathod2601@gmail.com>
2022-03-31 12:13:06 +0530

    Add

--- command/list_command.js
+++ command/list_command.js
@@ -4,11 +4,14 @@ const chalk=require('chalk')
 
 let list_obj={}
 
+list_obj["for showing all command"]="command"
 list_obj["for adding a module structure in API folder"]="command-addmodule"
 list_obj["for adding a authentication in your project"]="command-addauth"
+list_obj["for adding a module middlewAare multer"]="command-addfilemodule"
+list_obj["for adding multer globaly"]="command-addfile"
 
 
 
 for(let i in list_obj){
-    console.log(chalk.blue(i),"------>",chalk.green(list_obj[i]))
+    console.log(chalk.blue(i),"------>",chalk.green(list_obj[i]),"\n")
 }
